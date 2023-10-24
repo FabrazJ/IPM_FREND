595e46ffe5bd9ff5f214ce149f2dac2d6466efe7
FabrazJ <m.vargas.or10@gmail.com>
2023-10-24 12:27:17 -0500

    Actualizacion Front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { SidebarComponent } from './sidebarc/sidebar/sidebar.component';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'sidebar', component: SidebarComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Ruta predeterminada
+  { path: '**', redirectTo: '/login' }, // Ruta para manejar rutas no encontradas
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/sidebarc/sidebar/sidebar.component.ts
+++ src/app/sidebarc/sidebar/sidebar.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
