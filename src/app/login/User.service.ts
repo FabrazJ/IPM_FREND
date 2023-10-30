a7e08b61b057956d6024938858fe0311f9f5d4ed
FabrazJ <m.vargas.or10@gmail.com>
2023-10-30 15:28:55 -0500

    Actualizacion

---
+++ src/app/login/User.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string) {
+    const loginData = { email, password };
+
+    return this.http.post('http://192.168.100.28:5205/api/Usuario', loginData); 
+  }
+ 
+}
+ // constructor(private http: HttpClient) {}
+
+  // login(email: string, password: string) {
+  //   const loginData = { email, password };
+
+  //   return this.http.post('/api/login', loginData); // Sustituye la URL con la de tu servicio C#
+  // }
+
+ // private apiBaseURL = 'http://192.168.100.28:5205'; // Ajusta la IP y el puerto
+
+  // constructor(private http: HttpClient) {}
+
+  // getDatos() {
+  //   return this.http.get(`${this.apiBaseURL}/ruta-de-tu-api`);
+  // }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,64 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-
-
+import { UserService } from './User.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-//Hacerlo desde 0
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private authService: UserService, private router: Router) {
+  }
+  
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['michelle@example.com', [Validators.required, Validators.email]],
+      password: ['123', Validators.required],
+    });
+  }
+  
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const email = this.loginForm.get('email')?.value;
+      const password = this.loginForm.get('password')?.value;
+      
+      this.authService.login(email, password).subscribe(
+        (res: any) => {
+          if (res.success) {
+            alert('Inicio de sesión exitoso');
+            this.loginForm.reset();
+            this.router.navigate(['/sidebar']); // Redirigir a una página después del inicio de sesión exitoso
+          } else {
+            alert('Inicio de sesión fallido. Credenciales no válidas.');
+          }
+        },
+        (error) => {
+          console.error(error);
+          alert('Inicio de sesión fallido. Se produjo un error.');
+        }
+      );
+    } else {
+      alert('Por favor, complete todos los campos requeridos.');
+    }
+  }
+
+
 }
+
+// conexion que si da 
+
+//
+// datos:any;
+
+// constructor(private user:UserService){}
+// ngOnInit(): void {
+//   this.user.getDatos().subscribe((data: any) => {
+//     this.datos = data;
+//   });
+
 //implements OnInit {
 //   loginForm!: FormGroup;
 
