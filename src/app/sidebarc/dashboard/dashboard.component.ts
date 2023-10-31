d48de11e73b8d93862ea24c445bc37080e0ea123
FabrazJ <m.vargas.or10@gmail.com>
2023-10-31 11:53:40 -0500

    Actualizacion

--- src/app/sidebarc/dashboard/dashboard.component.ts
+++ src/app/sidebarc/dashboard/dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html'
 })
 export class DashboardComponent {
-
+ constructor(private route:Router){}
 }
