71f021c941872ec3f9ca085ed4a68a4d120b6416
FabrazJVargas <m.vargas.or10@gmail.com>
2023-11-08 14:31:26 -0500

    Overlay
    Agregado de Overlay en cerrar sesión y sweet alert

--- src/app/SideNav/header_nav/Componentes/CerrarSesion/CerrarSesion.component.ts
+++ src/app/SideNav/header_nav/Componentes/CerrarSesion/CerrarSesion.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ModalSesionService } from 'src/app/Servicios/ModalDataServiceHS/ModalSesion.service';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-CerrarSesion',
   templateUrl: './CerrarSesion.component.html',
@@ -10,7 +12,7 @@ export class CerrarSesionComponent  {
     
   isModalOpenSes:boolean = false;
 
-  constructor(private modalSesionS: ModalSesionService) {
+  constructor(private modalSesionS: ModalSesionService, private router:Router) {
     modalSesionS.isModalOpenSes$.subscribe(isOpen => 
       this.isModalOpenSes = isOpen);
   }
@@ -22,4 +24,19 @@ export class CerrarSesionComponent  {
   CloseSesModal() {
     this.isModalOpenSes = false;
   }
+
+  //SwitchAlert
+  Exit() {
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Exit',
+      showConfirmButton: false,
+      timer: 1500
+    }).then(() => {
+      // Redirigir al usuario a la página de inicio de sesión
+      this.router.navigate(['/login']); // Reemplaza 'login' con la ruta real de tu página de inicio de sesión
+    });
+  }
+  
 }
