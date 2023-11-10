fd6123af65415c783b4c219557e13f4f2a6453d6
FabrazJVargas <m.vargas.or10@gmail.com>
2023-11-10 10:42:06 -0500

    Quitar modal de configuracion
    Porque no es un modal, sino una pantalla que iria

--- src/app/SideNav/header_nav/Componentes/Configuracion/Configuracion.component.ts
+++ src/app/SideNav/header_nav/Componentes/Configuracion/Configuracion.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ModalConfiguracionService } from 'src/app/Servicios/ModalDataServiceHS/ModalConfiguracion.service';
+
 @Component({
   selector: 'app-Configuracion',
   templateUrl: './Configuracion.component.html',
@@ -8,19 +8,5 @@ import { ModalConfiguracionService } from 'src/app/Servicios/ModalDataServiceHS/
 
 export class ConfiguracionComponent {
 
-  isModalOpenConf:boolean = false;
-
-  constructor(private modalConf: ModalConfiguracionService) {
-    modalConf.isModalOpenConf$.subscribe(isOpen => 
-      this.isModalOpenConf = isOpen);
-  }
-
-  OpenConfModal() {
-    this.isModalOpenConf = true;
-  }
-
-  CloseConfModal() {
-    this.isModalOpenConf = false;
-  }
 
 }
